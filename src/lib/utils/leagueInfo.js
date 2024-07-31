bfc9fa1decb4152ea079842e11bf65a4d84fdf91
Richard 'Ricky' Kuehn <richardprinzkuehn@gmail.com>
2024-07-31 18:31:13 -0400

    delete photo link

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -32,14 +32,14 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Brooklyn, New York", // (optional)
       "bio": "sufjan steven fan account",
-      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": null, // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2018, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": null, // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Max", // Can be anything (usually your rival's name)
         link: 3, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        image: null, // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": null, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
