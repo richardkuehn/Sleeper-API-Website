56cde70dad7c24b87d347cdb69b1536aff04e192
Richard 'Ricky' Kuehn <richardprinzkuehn@gmail.com>
2024-07-31 21:36:24 -0400

    dave's photo

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -186,7 +186,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "St. Louis, MO", // (optional)
       "bio": "quitter",
-      "photo": "/managers/question.png", // square ratio recommended (no larger than 500x500)
+      "photo": "https://sleepercdn.com/uploads/0bac568ea3f2f16a15b982623f0dc730.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2018, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": 'lar'', // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
