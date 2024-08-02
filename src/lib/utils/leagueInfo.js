8466badc899f319a47033e72ec042c5146936317
Richard 'Ricky' Kuehn <richardprinzkuehn@gmail.com>
2024-08-02 19:26:31 -0400

    fix rival

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -126,7 +126,7 @@ export const managers = [
       "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Ricky", // Can be anything (usually your rival's name)
-        link: 1, // manager array number within this array, or null to link back to all managers page
+        link: 4, // manager array number within this array, or null to link back to all managers page
         image: "https://sleepercdn.com/uploads/af42604367d8dd39c550935f095f45d3.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 167, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
